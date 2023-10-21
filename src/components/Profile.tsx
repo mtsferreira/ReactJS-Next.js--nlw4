12bc30c9923193fab36dbc468f66000513bff118
mtsferreira <mateustutulilu@gmail.com>
2023-10-21 20:36:28 -0300

    Project Finished

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/mtsferreira.png" alt="Avatar" />
+            <div>
+                <strong>Mateus Ferreira</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level" />
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    )
+}
